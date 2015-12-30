13b90d5c4e3f240d1e5fd229daecd5c0d4caa657
Harry McKillen <harry@wolfhound.ie>
2015-12-30 13:35:11 +0000

    Convert to grunt project

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      build: {
+        files: [
+          { src: 'src/index.html', dest: 'build/index.html' },
+          // { src: '../yafgs/css/yafgs.max.css', dest: 'build/css/yafgs.max.css' },
+          {expand: true, cwd: 'src/', src: ['font/**'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['coderdojo/**'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['imgs/**'], dest: 'build/'}
+        ]
+      },
+    },
+    sass: {
+      options: {
+        style: 'compressed',
+        sourcemap: 'none'
+      },
+      build: {
+        files: {
+          'build/css/style.min.css': 'src/sass/style.scss'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          hostname: 'localhost',
+          port: 4000,
+          base: 'build/',
+          open: true
+        }
+      }
+    },
+    clean: {
+      build: {
+        src: ['build/']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build',
+    [
+      'clean:build',
+      'sass:build',
+      'copy:build'
+    ]);
+  grunt.registerTask('default',
+    [
+      'build'
+    ]);
+  grunt.registerTask('serve',
+    [
+      'build',
+      'connect'
+    ]);
+};
---
+++ README.md
@@ -0,0 +1,56 @@
+# Ireland Glyph Font Regular
+
+This font has been created for a bit of fun. Each of the 32 counties are included, along with the map of Ireland. 
+
+## Demo
+If you want to see a demo of the font <a href="http://harrymckillen.github.io/ireland-font/" target="_blank">please visit this sample page here.</a>
+
+## Usage
+
+Include the `css/ireland-glyph-font.max.css` file (or choose the *.min.css version) in your HTML file and ensure you have the paths to the font files correct, depending on your setup. 
+
+The follow classes are available:
+
+```
+icon-ireland
+icon-antrim
+icon-armagh
+icon-carlow
+icon-cavan
+icon-clare
+icon-cork
+icon-derry
+icon-donegal
+icon-down
+icon-dublin
+icon-fermanagh
+icon-galway
+icon-kerry
+icon-kildare
+icon-kilkenny
+icon-laois
+icon-leitrim
+icon-limerick
+icon-longford
+icon-louth
+icon-mayo
+icon-meath
+icon-monaghan
+icon-offaly
+icon-roscommon
+icon-sligo
+icon-tipperary
+icon-tyrone
+icon-waterford
+icon-westmeath
+icon-wexford
+icon-wicklow
+```
+
+## License 
+
+SIL Open Font License (OFL) 
+
+Feel free to use anywhere, commerical or non-commercial, no attribution is required, but any is welcome. 
+
+.
