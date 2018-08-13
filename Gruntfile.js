b4f35f3c8dba9903ff7f4446d2b96ed43b03f491
Harry McKillen <harry@wolfhound.ie>
2018-08-13 06:13:56 +0100

    Kick off new design

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,38 @@
+/*jslint
+    white */
+/* global
+  grunt */
 module.exports = function (grunt) {
+  'use script';
+
+  // Configs
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     copy: {
       build: {
         files: [
-          { src: 'src/index.html', dest: 'build/index.html' },
-          {expand: true, cwd: 'src/', src: ['js/**'], dest: 'build/'},
           {expand: true, cwd: 'src/', src: ['font/**'], dest: 'build/'},
-          {expand: true, cwd: 'src/', src: ['coderdojo/**'], dest: 'build/'},
-          {expand: true, cwd: 'src/', src: ['imgs/**'], dest: 'build/'}
+          {expand: true, cwd: 'src/', src: ['img/**'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['js/**'], dest: 'build/'}
         ]
-      },
+      }
     },
     sass: {
-      options: {
-        style: 'compressed',
-        sourcemap: 'none'
-      },
       build: {
-        files: {
-          'build/css/style.min.css': 'src/sass/style.scss'
-        }
+        options: {
+          implementation: 'sass',
+          style: 'compressed',
+          sourcemap: 'none'
+        },
+        files:  [
+          {
+            expand: true,
+            cwd: 'src/sass',
+            src: ['**/*.scss', "!libs/*.scss", "!components/*.scss"],
+            dest: 'build/css',
+            ext: '.css'
+          }
+        ]
       }
     },
     connect: {
@@ -34,21 +46,69 @@ module.exports = function (grunt) {
         }
       }
     },
+    pug: {
+      build: {
+        options: {
+          pretty: false
+        },
+        files: [
+          {
+            cwd: "src",
+            src: ["**/*.pug", "!templates/**/*.pug"],
+            dest: "build",
+            expand: true,
+            ext: ".html"
+          }
+        ]
+      }
+    },
     clean: {
       build: {
         src: ['build/']
       }
+    },
+    watch: {
+      options: { livereload: true },
+      files: ['src/**/*.pug', 'src/**/*.js', 'src/**/*.sass'],
+      tasks: ['build']
+    },
+    processhtml: {
+      build: {
+        options: {
+          process: true,
+          data: {
+            sitetitle: 'Wolfhound Media Design Demos'
+          }
+        },
+        files: [
+          {expand: true, cwd: 'build/', src: ['*.html'], dest: 'build/'}
+        ]
+      }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-ftp-push');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+
+  // Registered Tasks
   grunt.registerTask('build',
     [
       'clean:build',
       'sass:build',
-      'copy:build'
+      'copy:build',
+      'pug:build'
+    ]);
+  grunt.registerTask('wipe',
+    [
+      'clean:build'
     ]);
   grunt.registerTask('default',
     [
@@ -57,6 +117,33 @@ module.exports = function (grunt) {
   grunt.registerTask('serve',
     [
       'build',
-      'connect'
+      'watch'
     ]);
+
+  // FTP transfer task
+  grunt.registerTask('deploy', 'A simple task that ftp\'s stuff.', function (){
+
+    var hosts = grunt.file.readJSON('hosts.json');
+    grunt.initConfig({
+      ftp_push: {
+        options: {
+          authKey: "dev",
+          host: hosts.dev.remoteurl,
+          dest: hosts.dev.remotedir,
+          port: 21,
+          debug: false
+        },
+        full: {
+          files: [
+            {expand: true, cwd: 'build', src: ['**/*', '.htaccess']}
+          ]
+        }
+      }
+    });
+
+    grunt.task.run([
+      'ftp_push:full'
+    ]);
+
+  });
 };
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# harrymckillen.com
+# Harry McKillen Dot Com
 
-Repo for <a href="http://www.harrymckillen.com/" target="_blank">harrymckillen.com</a>. This is my personal website.
+Based off my grunt skeleton project repo, this seeks to be a test bed for some of the latest web technologies. For this I will not be focusing on browser support. So things might not work well in every browser, and it's by design than omission. 
+
+
+It has a few grunt modules install and pre-configured out-of-the-box. 
+
+* Pug (formerly Jade)
+* Sass - CSS preprocessor I tend to favour. 
+* Basic FTP module for deploying your stuff to a server
+* A few other basic npm modules which support the building of a 'dist' version of a website. 
