d4523efb90984e39495c7ea2f5988b03273766db
Harry McKillen <harry@wolfhound.ie>
2019-02-17 14:57:43 +0000

    Added new theme colours, clean up code, and add a few js utils

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,3 +1,68 @@
+// js utils
+
+// basic cookie management
+
+function setCookie(name, value, expiry) {
+  var d = new Date();
+  d.setTime(d.getTime() + (expiry*24*60*60*1000));
+  var expires = 'expires='+ d.toUTCString();
+  document.cookie = name + '=' + value + ';' + expires + ';path=/';
+}
+
+function getCookie(name) {
+  var cookie = name + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookieArray = decodedCookie.split(';');
+  for(var i = 0; i < cookieArray.length; i++) {
+    var c = cookieArray[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(cookie) == 0) {
+      return c.substring(cookie.length, c.length);
+    }
+  }
+  return '';
+}
+
+// binds to any element with the class theme-button
+var classname = document.getElementsByClassName('theme-button');
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', changeTheme, false);
+}
+
+// if a cookie exists, sets that style, if not, sets the default style
+function themeCookie() {
+  var theme = getCookie('theme');
+  if (theme != '') {
+   document.querySelector('body').setAttribute('class', theme);
+  } else {
+    setCookie('theme', 'default', 30);
+  }
+}
+
+// theme changer extraordinaire
+var changeTheme = function() {
+    var attribute = this.getAttribute('data-themeid');
+    document.querySelector('body').setAttribute('class', attribute);
+    setCookie('theme', attribute, 30);
+};
+
+
+// small function to provide simulated blinking for a cursor
+function blinker() {
+  var cursor = document.getElementsByClassName('blinking-cursor');
+  setInterval(function() {
+    if(cursor[0].classList.contains('show')){
+      cursor[0].classList.remove('show');
+    }
+    else {
+      cursor[0].classList.add('show');
+    }
+  }, 700);
+}
+
+// google analytics
 var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-23295796-1']);
   _gaq.push(['_trackPageview']);
@@ -15,3 +80,9 @@ baffle('.main-title')
       speed: 150
   }
   ).reveal(2500);
+
+// run the themeCookie function
+themeCookie();
+
+// run the blinking function
+blinker();
