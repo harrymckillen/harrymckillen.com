87defb392a75a2d9a16a7aaed0106a2854fbf2e1
Harry McKillen <1691128+harrymckillen@users.noreply.github.com>
2023-08-16 11:04:55 +0100

    Create  Header components

---
+++ components/header/HeaderNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="nav-wrapper">
+    <nav
+      class="flex items-center h-16 w-full sm:w-2/3 justify-center mx-auto text-lg sm:text-2xl space-x-8 sm:space-x-12"
+    >
+      <a href="#about">About</a><a href="#skills">Skills</a
+      ><a href="#contact">Contact</a>
+    </nav>
+  </div>
+</template>
